e3dbaa4ae969ac29b4a6dffbab73813c8e5be007
055055 <snb4301@gmail.com>
2019-12-08 14:54:08 +0900

    라우터 인스턴스

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-
-const Login = { template: '<div>Login Page</div>' }
-const NotFound = { template: '<div>Page not found</div>' }
-
-
-const router = new VueRouter({
-    mode: 'history', //url # 제거. 
-    routes: [
-        { path: '/', component: App },
-        { path: '/login', component: Login },
-        { path: '*', component: NotFound } //마지막은 다 여기로. if else와 같이
-    ]
-})
-
+import router from './router'
 
 new Vue({
     el: '#app',

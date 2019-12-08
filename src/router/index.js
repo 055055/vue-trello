419fef7ba2cf3a025374a66ac3fce14f0db7f5b6
055055 <snb4301@gmail.com>
2019-12-08 15:35:45 +0900

    동적 라우트 매칭

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import Vue from 'vue'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import NotFound from '../components/NotFound.vue'
+import Board from '../components/Board.vue'
 
 
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter) //이거 때문에 $route로 라우터 접근 가능. 
 
 
 const router = new VueRouter({
@@ -15,6 +15,7 @@ const router = new VueRouter({
     routes: [
         { path: '/', component: Home },
         { path: '/login', component: Login },
+        { path: '/b/:bid', component: Board }, //: bid라는 변수로 받을 수 있다.
         { path: '*', component: NotFound } //마지막은 다 여기로. if else와 같이
     ]
 })

b5ef5f8858274b3a6ef9eddff855d79b6eeb7d91
055055 <snb4301@gmail.com>
2019-12-08 13:04:43 +0900

    Vue-router

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const Login = { template: '<div>Login Page</div>' }
 const NotFound = { template: '<div>Page not found</div>' }
 
-const routes = {
-    '/': App,
-    '/login': Login
-}
+
+const router = new VueRouter({
+    mode: 'history', //url # 제거. 
+    routes: [
+        { path: '/', component: App },
+        { path: '/login', component: Login },
+        { path: '*', component: NotFound } //마지막은 다 여기로. if else와 같이
+    ]
+})
+
 
 new Vue({
     el: '#app',
-    computed: {
-        VueComponent() {
-            return routes[window.location.pathname] || NotFound
-        }
-    },
-    render(h) {
-        return h(this.VueComponent)
-    }
+    router,
+    render: h => h({ template: '<router-view></router-view>' })
 })

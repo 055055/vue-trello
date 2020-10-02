354869ef70b19a38bbaa0e1c1b7e93b2fc643f64
055055 <snb4301@gmail.com>
2020-10-02 17:32:27 +0900

    Vuex 적용 - 인증 1

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,13 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
         isAddBoard: false,
-        boards: []
+        boards: [],
+        token: null
+    },
+    getters: {
+        isAuth(state) {
+            return !!state.token
+        }
     },
     mutations: {
         SET_IS_ADD_BOARD(state, toggle) {
@@ -15,8 +21,20 @@ const store = new Vuex.Store({
         },
         SET_BOARDS(state, boards) {
             state.boards = boards
+        },
+        LOGIN(state, token) {
+            if (!token) return
+            state.token = token
+            localStorage.setItem('token', token)
+            api.setAuthInHeader(token)
+        },
+        LOGOUT(state) {
+            state.token = null
+            delete localStorage.token
+            api.setAuthInHeader(null)
         }
     },
+    //action은 api 호출같은 비동기 호출을 담당 mutation은 동기
     actions: {
         ADD_BOARD(_, { title }) {
             return api.board.create(title)
@@ -25,9 +43,17 @@ const store = new Vuex.Store({
             return api.board.fetch().then(data => {
                 commit('SET_BOARDS', data.list)
             })
+        },
+        LOGIN({ commit }, { email, password }) {
+            return api.auth.login(email, password)
+                .then(({ accessToken }) => commit('LOGIN', accessToken))
         }
     }
 })
+
+const { token } = localStorage
+store.commit('LOGIN', token)
+
 
 
 export default store

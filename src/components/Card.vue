f369f04b29ca75c6605d18700189bde22b4fe4f0
055055 <snb4301@gmail.com>
2020-10-12 23:03:30 +0900

    카드 삭제

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,75 +2,86 @@
   <Modal class="modal-card">
     <div slot="header" class="modal-card-header">
       <div class="modal-card-header-title">
-        <input class="form-control" type="text" :value="card.title" :readonly="!toggleTitle" 
-        @click="toggleTitle=true" @blur="onBlurTitle"
-        ref="inputTitle">
+        <input
+          class="form-control"
+          type="text"
+          :value="card.title"
+          :readonly="!toggleTitle"
+          @click="toggleTitle=true"
+          @blur="onBlurTitle"
+          ref="inputTitle"
+        />
       </div>
-      <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
+      <a class="modal-close-btn" href @click.prevent="onClose">&times;</a>
     </div>
     <div slot="body">
       <h3>Description</h3>
-      <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
-        :readonly="!toggleDesc" @click="toggleDesc=true" @blur="onBlurDesc"
+      <textarea
+        class="form-control"
+        cols="30"
+        rows="3"
+        placeholder="Add a more detailed description..."
+        :readonly="!toggleDesc"
+        @click="toggleDesc=true"
+        @blur="onBlurDesc"
         ref="inputDesc"
-        v-model="card.description"></textarea>
+        v-model="card.description"
+      ></textarea>
     </div>
     <div slot="footer"></div>
   </Modal>
 </template>
 
 <script>
-import Modal from './Modal.vue'
-import {mapActions, mapState} from 'vuex'
+import Modal from "./Modal.vue";
+import { mapActions, mapState } from "vuex";
 
 export default {
-    components: {
-        Modal
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      toggleTitle: false,
+      toggleDesc: false
+    };
+  },
+  computed: {
+    ...mapState({
+      card: "card",
+      board: "board"
+    })
+  },
+  created() {
+    this.fetchCard();
+  },
+  methods: {
+    ...mapActions(["FETCH_CARD", "UPDATE_CARD"]),
+    onClose() {
+      this.$router.push(`/b/${this.board.id}`);
     },
-    data() {
-        return {
-            toggleTitle: false,
-            toggleDesc: false
-        }
+    fetchCard() {
+      const id = this.$route.params.cid;
+      this.FETCH_CARD({ id });
     },
-    computed:{
-        ...mapState({
-            card: 'card',
-            board: 'board'
-        })
-    },
-    created(){
+    onBlurTitle() {
+      this.toggleTitle = false;
+      const title = this.$refs.inputTitle.value.trim();
+      if (!title) return;
+      this.UPDATE_CARD({ id: this.card.id, title }).then(() =>
         this.fetchCard()
+      );
     },
-    methods: {
-        ...mapActions([
-            'FETCH_CARD',
-            'UPDATE_CARD'
-        ]),
-        onClose() {
-          this.$router.push(`/b/${this.board.id}`)
-        },
-        fetchCard(){
-          const id = this.$route.params.cid
-          this.FETCH_CARD({id})
-        },
-        onBlurTitle(){
-            this.toggleTitle = false
-            const title = this.$refs.inputTitle.value.trim()
-            if (!title) return
-            this.UPDATE_CARD({id: this.card.id, title})
-               .then(() => this.fetchCard())
-        },
-        onBlurDesc(){
-            this.toggleDesc = false
-            const description = this.$refs.inputDesc.value.trim()
-            if (!description) return
-            this.UPDATE_CARD({id: this.card.id, description})
-              .then(()=> this.fetchCard())   
-        }
+    onBlurDesc() {
+      this.toggleDesc = false;
+      const description = this.$refs.inputDesc.value.trim();
+      if (!description) return;
+      this.UPDATE_CARD({ id: this.card.id, description }).then(() =>
+        this.fetchCard()
+      );
     }
-     
-}
+  }
+};
 </script>
 
 <style>
@@ -80,7 +91,7 @@ export default {
   width: 60%;
 }
 .modal-card-header-title {
-  padding-right: 30px;  
+  padding-right: 30px;
 }
 .modal-close-btn {
   position: absolute;
